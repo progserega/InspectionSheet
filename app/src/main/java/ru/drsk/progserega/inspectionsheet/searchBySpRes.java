f0d5a328f7d2ca2f123643264708723837b3087f
Семёнов Сергей <semenov@rsprim.ru>
2018-07-13 16:54:31 +1000

    добавляю активности

---
+++ app/src/main/java/ru/drsk/progserega/inspectionsheet/searchBySpRes.java
@@ -0,0 +1,13 @@
+package ru.drsk.progserega.inspectionsheet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class searchBySpRes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_by_sp_res);
+    }
+}
