4cdd18599a6e1b745e5fa8de71bbf9c31ddac462
Семёнов Сергей <semenov@rsprim.ru>
2018-07-20 10:49:25 +1000

    закончил добавлять активности

---
+++ app/src/main/java/ru/drsk/progserega/inspectionsheet/EndInspection.java
@@ -0,0 +1,13 @@
+package ru.drsk.progserega.inspectionsheet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EndInspection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_inspection);
+    }
+}
